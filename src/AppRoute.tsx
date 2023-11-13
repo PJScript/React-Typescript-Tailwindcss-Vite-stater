821cee4386e011c31436c53e356caf7dd095b836
PJScript <74460103+PJScript@users.noreply.github.com>
2023-11-14 00:57:22 +0900

    README

--- src/AppRoute.tsx
+++ src/AppRoute.tsx
@@ -8,15 +8,13 @@ const AppRoute: () => JSX.Element = () => {
   return (
     <>
       <StickyNavbar />
-      <div className="flex w-[100%] flex-col items-center justify-center px-4 py-2 lg:px-8 lg:py-4">
+      <div
+        className="flex w-[100%] flex-col 
+      items-center justify-center px-4 py-2 lg:px-8 lg:py-4"
+      >
         <Routes>
           {PAGE_LIST.map(({ path, children }) => (
-            <Route
-              path={path}
-              element={
-                <div className="w-[100%] max-w-screen-xl">{children}</div>
-              }
-            />
+            <Route path={path} element={<div className="w-[100%] max-w-screen-xl">{children}</div>} />
           ))}
         </Routes>
       </div>
