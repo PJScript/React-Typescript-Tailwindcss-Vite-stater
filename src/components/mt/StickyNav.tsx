0bbe53b933e3d7357b1a290094faf4f8dd4c71bf
PJScript <74460103+PJScript@users.noreply.github.com>
2023-11-17 20:34:16 +0900

    test

--- src/components/mt/StickyNav.tsx
+++ src/components/mt/StickyNav.tsx
@@ -1,10 +1,4 @@
-import {
-  Button,
-  IconButton,
-  MobileNav,
-  Navbar,
-  Typography,
-} from '@material-tailwind/react';
+import { Button, Collapse, IconButton, Navbar, Typography } from '@material-tailwind/react';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -12,20 +6,13 @@ const StickyNavbar = (): JSX.Element => {
   const [openNav, setOpenNav] = React.useState(false);
 
   React.useEffect(() => {
-    window.addEventListener(
-      'resize',
-      () => window.innerWidth >= 960 && setOpenNav(false),
-    );
+    window.addEventListener('resize', () => window.innerWidth >= 960 && setOpenNav(false));
   }, []);
 
   const navList = (
-    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
       <Link to="/">
-        <Typography
-          variant="small"
-          color="blue-gray"
-          className="p-1 font-normal"
-        >
+        <Typography variant="small" color="blue-gray" className="p-1 font-normal">
           home
         </Typography>
       </Link>
@@ -33,33 +20,23 @@ const StickyNavbar = (): JSX.Element => {
   );
 
   return (
-    <div className="flex justify-center max-h-[768px] overflow-auto border-b-2 px-4 py-2 lg:px-8 lg:py-4 ">
-      <Navbar className="sticky top-0 z-10 h-max rounded-none shadow-none max-w-screen-xl  px-0 py-0 lg:px-0 lg:py-0">
+    <div className="flex max-h-[768px] justify-center overflow-auto border-b-2 px-4 py-2 lg:px-8 lg:py-4 ">
+      <Navbar className="sticky top-0 z-10 h-max max-w-screen-xl rounded-none px-0  py-0 shadow-none lg:px-0 lg:py-0">
         <div className="flex items-center justify-between text-blue-gray-900 ">
           <Link to="/">
-            <Typography className="mr-4 cursor-pointer py-1.5 font-medium">
-              Title
-            </Typography>
+            <Typography className="mr-4 cursor-pointer py-1.5 font-medium">Title</Typography>
           </Link>
 
-          <div className="items-center gap-4 flex">
+          <div className="flex items-center gap-4">
             <div className="mr-4 hidden lg:block">{navList}</div>
             <div className="flex items-center gap-x-1">
               <Link to="/login">
-                <Button
-                  variant="text"
-                  size="sm"
-                  className="hidden lg:inline-block"
-                >
+                <Button variant="text" size="sm" className="hidden lg:inline-block">
                   <span>Log In</span>
                 </Button>
               </Link>
               <Link to="/signup">
-                <Button
-                  variant="gradient"
-                  size="sm"
-                  className="hidden lg:inline-block"
-                >
+                <Button variant="gradient" size="sm" className="hidden lg:inline-block">
                   <span>Sign Up</span>
                 </Button>
               </Link>
@@ -71,39 +48,18 @@ const StickyNavbar = (): JSX.Element => {
               onClick={(): void => setOpenNav(!openNav)}
             >
               {openNav ? (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  className="h-6 w-6"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" className="h-6 w-6" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
                 </svg>
               ) : (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" stroke="currentColor" strokeWidth={2}>
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
                 </svg>
               )}
             </IconButton>
           </div>
         </div>
-        <MobileNav open={openNav}>
+        <Collapse open={openNav}>
           {navList}
           <div className="flex items-center gap-x-1">
             <Button fullWidth variant="text" size="sm" className="">
@@ -113,7 +69,7 @@ const StickyNavbar = (): JSX.Element => {
               <span>Sign in</span>
             </Button>
           </div>
-        </MobileNav>
+        </Collapse>
       </Navbar>
     </div>
   );
