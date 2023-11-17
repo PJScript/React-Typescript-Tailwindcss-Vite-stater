c764560ed765ea2635f8a9f8f7b3590c3355fe07
PJScript <74460103+PJScript@users.noreply.github.com>
2023-11-17 20:50:28 +0900

    로컬에서 사라진 내용 복구

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,21 @@
+import UserList from '../components/UserList';
+import { ApiErrorBoundary } from '../components/error/ApiErrorBoundary';
+
 const Home = (): JSX.Element => {
-  return <div>Home</div>;
+  // if (isLoading) {
+  //   return <div>LOADING...</div>;
+  // }
+
+  // if (isError) {
+  //   return <div>Opps.. throw ERROR</div>;
+  // }
+  return (
+    <div>
+      <ApiErrorBoundary>
+        <UserList />
+      </ApiErrorBoundary>
+    </div>
+  );
 };
+
 export default Home;
