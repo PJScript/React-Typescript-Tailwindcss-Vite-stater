395f29c0c60b81c8f0312fa125a060c003f0b67c
PJScript <pjs771188@gmail.com>
2023-11-21 17:46:42 +0900

    commit test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import { ApiErrorBoundary } from '../components/error/ApiErrorBoundary';
 
 const Home = (): JSX.Element => {
   console.log('test');
+  console.log('123');
   // if (isLoading) {
   //   return <div>LOADING...</div>;
   // }
